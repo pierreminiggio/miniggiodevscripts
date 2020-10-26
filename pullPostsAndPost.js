92ec51b5f7a315c093cf986c3c24810d8910b7b2
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-26 15:24:27 +0100

    space4

--- pullPostsAndPost.js
+++ pullPostsAndPost.js
@@ -6,90 +6,90 @@ let ids = require('./ids.js');
 
 const pullPost = async (ids) => {
 
-  // Créer une instance de navigateur
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    // Créer une instance de navigateur
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  // Naviguer jusqu'à l'URL cible
-  await page.goto(ids.url);
+    // Naviguer jusqu'à l'URL cible
+    await page.goto(ids.url);
 
-  // Récupérer les données
-  var post = await page.evaluate(() => {
-    let daPost = document.querySelector('pre').innerHTML;
-    return JSON.parse(daPost);
-  });
+    // Récupérer les données
+    var post = await page.evaluate(() => {
+        let daPost = document.querySelector('pre').innerHTML;
+        return JSON.parse(daPost);
+    });
 
-  // Retourner les données (et fermer le navigateur)
-  browser.close();
-  return post;
+    // Retourner les données (et fermer le navigateur)
+    browser.close();
+    return post;
 }
 
 const postToPage = async (ids, qualityContent) => {
 
-  // Créer une instance de navigateur
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    // Créer une instance de navigateur
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  // Naviguer jusqu'à l'URL cible
-  await page.goto('https://www.facebook.com/');
-  
-  // Connexion si pas connecté
-  var login = await page.evaluate((ids) => {
-    document.querySelector('#email').value = ids.mail;
-    document.querySelector('#pass').value = ids.pass;
-  }, ids);
+    // Naviguer jusqu'à l'URL cible
+    await page.goto('https://www.facebook.com/');
+    
+    // Connexion si pas connecté
+    var login = await page.evaluate((ids) => {
+        document.querySelector('#email').value = ids.mail;
+        document.querySelector('#pass').value = ids.pass;
+    }, ids);
 
-  await page.click(
-    '#loginbutton',
-  )
-  await page.waitFor(1000); // fait une pause d'une seconde
+    await page.click(
+        '#loginbutton',
+    )
+    await page.waitFor(1000); // fait une pause d'une seconde
 
-  // On va sur le groupe des boosted
-  await page.goto('https://www.facebook.com/'+ids.page+'/');
+    // On va sur le groupe des boosted
+    await page.goto('https://www.facebook.com/'+ids.page+'/');
 
 
-  await page.waitFor(1000); // fait une pause d'une seconde
+    await page.waitFor(1000); // fait une pause d'une seconde
 
-  // use keyboard shortcut to open and focus on create new post
-  await page.keyboard.press('KeyP');
+    // use keyboard shortcut to open and focus on create new post
+    await page.keyboard.press('KeyP');
 
-  // wait for emoji icon as proxy for "loaded and post ready"
-  await page.waitFor('button[data-testid="react-composer-post-button"][type="submit"]');
+    // wait for emoji icon as proxy for "loaded and post ready"
+    await page.waitFor('button[data-testid="react-composer-post-button"][type="submit"]');
 
-  // keyboard shortcut put focus in place so we can just type
-  await page.keyboard.type(qualityContent);
+    // keyboard shortcut put focus in place so we can just type
+    await page.keyboard.type(qualityContent);
 
-  // click submit
-  await page.click('button[data-testid="react-composer-post-button"][type="submit"]');
+    // click submit
+    await page.click('button[data-testid="react-composer-post-button"][type="submit"]');
 
-   await page.waitFor(3000); // fait une pause de 3 secondes
+     await page.waitFor(3000); // fait une pause de 3 secondes
 
-  // Retourner les données (et fermer le navigateur)
-  browser.close();
-  return [];
+    // Retourner les données (et fermer le navigateur)
+    browser.close();
+    return [];
 }
 
 function letsGo () {
-  console.log('Récupération du post');
+    console.log('Récupération du post');
 
-  // Appelle la fonction pullPost() et affichage les données retournées
-  pullPost(ids).then(post => {
-    console.log('post récupéré');
-    if (post.text != undefined) {
-      var qualityContent = post.text;
-      console.log(qualityContent);
+    // Appelle la fonction pullPost() et affichage les données retournées
+    pullPost(ids).then(post => {
+        console.log('post récupéré');
+        if (post.text != undefined) {
+            var qualityContent = post.text;
+            console.log(qualityContent);
 
-      // Postage sur la meilleure page de shitposting
-      postToPage(ids, qualityContent).then(result => {
-        console.log('post posté');
-      });
+            // Postage sur la meilleure page de shitposting
+            postToPage(ids, qualityContent).then(result => {
+                console.log('post posté');
+            });
 
-    }
-  });
+        }
+    });
 }
 
 letsGo();
 // Tous les 3mn
 cron.schedule('* * * * *', () => {
-  letsGo();
+    letsGo();
 });
